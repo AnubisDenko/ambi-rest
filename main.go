6e4232e298e919d0b5d25d6dbbdee4859a997826
Oliver Refle <orefle@gmail.com>
2018-09-01 23:19:06 +0800

    correclty log body

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+//const tokenRequestUrl = "https://ambi-rest.herokuapp.com/"
 const tokenRequestUrl = "https://api.ambiclimate.com/oauth2/authorize?client_id=cHKV&redirect_uri=https%3A%2F%2Fambi-rest.herokuapp.com%2Fsecret&response_type=code"
 func main() {
 	port := os.Getenv("PORT")
-
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
@@ -19,21 +21,35 @@ func main() {
 
 	router.GET("/", SayHello)
 	router.GET("/secret", ReceiveSecret)
-
-	RequestToken()
+	//RequestToken()
 	router.Run(":" + port)
 
 
 }
 
 func RequestToken(){
-	log.Println("Sending Token Request")
-	resp, err := http.Get(tokenRequestUrl)
-	if  err != nil {
-		log.Fatal(err)
-	}
 
-	log.Println(resp.Request)
+	timer:= time.NewTimer(2 * time.Second)
+	go func() {
+		<-timer.C
+		log.Println("Sending Token Request")
+		resp, err := http.Get(tokenRequestUrl)
+		if  err != nil {
+			log.Fatal("Error",err)
+		}
+
+		if resp == nil {
+			log.Fatal("No response")
+		}
+		ReadBody(resp.Body)
+	}()
+}
+
+func ReadBody(body io.ReadCloser){
+	buf := make([]byte, 1024)
+	length,_  := body.Read(buf)
+	bodyAsString := string(buf[0:length])
+	log.Println("Body",bodyAsString)
 }
 
 
